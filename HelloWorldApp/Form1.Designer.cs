5c04edd7f97833bcdaf9072a6107147f24dc80e8
prodigygiga <prodigygiga@gmail.com>
2021-10-06 02:16:20 +0400

    test commit

---
+++ HelloWorldApp/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace HelloWorldApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_number1 = new System.Windows.Forms.TextBox();
+            this.txt_number2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_sum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_number1
+            // 
+            this.txt_number1.Location = new System.Drawing.Point(42, 23);
+            this.txt_number1.Name = "txt_number1";
+            this.txt_number1.Size = new System.Drawing.Size(100, 23);
+            this.txt_number1.TabIndex = 0;
+            this.txt_number1.TextChanged += new System.EventHandler(this.txt_number1_TextChanged);
+            // 
+            // txt_number2
+            // 
+            this.txt_number2.Location = new System.Drawing.Point(185, 23);
+            this.txt_number2.Name = "txt_number2";
+            this.txt_number2.Size = new System.Drawing.Size(100, 23);
+            this.txt_number2.TabIndex = 1;
+            this.txt_number2.TextChanged += new System.EventHandler(this.txt_number2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(156, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "+";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(301, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "=";
+            // 
+            // lbl_sum
+            // 
+            this.lbl_sum.AutoSize = true;
+            this.lbl_sum.Location = new System.Drawing.Point(335, 26);
+            this.lbl_sum.Name = "lbl_sum";
+            this.lbl_sum.Size = new System.Drawing.Size(13, 15);
+            this.lbl_sum.TabIndex = 4;
+            this.lbl_sum.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 450);
+            this.Controls.Add(this.lbl_sum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_number2);
+            this.Controls.Add(this.txt_number1);
+            this.Name = "Form1";
+            this.Text = "miniCalc";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_number1;
+        private System.Windows.Forms.TextBox txt_number2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_sum;
+    }
+}
+
---
+++ HelloWorldApp/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorldApp
+{
+    public partial class Form1 : Form
+    {
+        int sum = 0;
+        int a = 0;
+        int b = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string k = "test";
+            float f = 10.5f;
+            double d = 15.2;
+            decimal dc = 16.2313m;
+            
+            if (f == 10.5f)
+            {
+
+            }
+
+        }
+
+        private void txt_number1_TextChanged(object sender, EventArgs e)
+        {
+            if (txt_number1.Text == "")
+            {
+                sum -= a;
+                lbl_sum.Text = sum.ToString();
+                //MessageBox.Show("შემოიტანეთ რიცხვი 1!");
+            }
+            else
+            {
+                a = Convert.ToInt32(txt_number1.Text);
+                sum += a;
+                lbl_sum.Text = sum.ToString();
+            }
+
+        }
+
+        private void txt_number2_TextChanged(object sender, EventArgs e)
+        {
+            if (txt_number2.Text == "")
+            {
+                sum -= b;
+                lbl_sum.Text = sum.ToString();
+                //MessageBox.Show("შემოიტანეთ რიცხვი 2!");
+            }
+            else
+            {
+                b = Convert.ToInt32(txt_number2.Text);
+                sum += b;
+                lbl_sum.Text = sum.ToString();
+            }
+            
+        }
+    }
+}
