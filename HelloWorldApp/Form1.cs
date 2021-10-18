9324facf8a2151f13abfcd51424f86db1c95b41b
Giga Grigalashvili <prodigygiga@gmail.com>
2021-10-19 01:48:17 +0400

    cmt

--- HelloWorldApp/Form1.cs
+++ HelloWorldApp/Form1.cs
@@ -12,9 +12,9 @@ namespace HelloWorldApp
 {
     public partial class Form1 : Form
     {
-        int sum = 0;
-        int a = 0;
-        int b = 0;
+        //int sum = 0;
+        //int a = 0;
+        //int b = 0;
         public Form1()
         {
             InitializeComponent();
@@ -22,96 +22,168 @@ namespace HelloWorldApp
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //int a = 5;
+            List<Person> people = new List<Person>();
+            //int sum = 0;
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    if (i % 5 == 0)
+            //    {
+            //        continue;
+            //    }
+            //    sum += i;
 
 
-            // stack  -  value types      - Immutable way string
-            // int , bool, double, float
+            //}
+            //MessageBox.Show(sum.ToString());
 
-            //heap  -  reference types    mutable way  - string
-            // string, class , object
-
-
-            //int b = 5;
-            //int c = b;
-            //b = 3;
-            //int d = ++c;
-            //c = 10;
-            //MessageBox.Show("c = " + c.ToString());
-
+            //int x = int.MaxValue;
+            //MessageBox.Show(x.ToString());
+            //int y = x + 1;
+            //MessageBox.Show(y.ToString());
             Person p1 = new Person();
-            p1.FirstName = "giorgi";
-            p1.LastName = "gogokhia";
-            p1.BirthDate = new DateTime(1997, 7, 9);
+            p1.FirstName = "dea";
+            p1.LastName = "kapanadze";
+            p1.BirthDate = new DateTime(1997, 1, 8);
+            p1.Height = 170;
+            int deaAge = p1.CalculatePersonAge();
+            people.Add(p1);
+            //MessageBox.Show(deaAge.ToString());
+            //p1.PrintInfo();
+            //p1.PrintInfo("GE");
+            //p1.PrintInfo(2423);
+            //MessageBox.Show(p1.IsOddCharsInFirstName().ToString());
+
+
 
             Person p2 = new Person();
-            p2.FirstName = p1.FirstName;
+            p2.FirstName = "nato";
+            p2.LastName = "labadze";
+            p2.BirthDate = new DateTime(1997, 10, 14);
+            p2.Height = 170;
+            people.Add(p2);
 
-            
-            p1.LastName = "dea";
+            Person p3 = new Person("giorgi", "gogokhia", 186, new DateTime(1997, 10, 20));
+            string result = p3.GetPrintData("GE");
+            int age = p3.CalculatePersonAge();
+            int charCount = p3.CalculateCharsInPerson();
+            people.Add(p3);
+            //MessageBox.Show(p3.IsOddCharsInFirstName().ToString());
 
 
-            MessageBox.Show("FirstName = " + p2.FirstName);
+            foreach (var item in people)
+            {
+                if (!item.IsOddCharsInFirstName())
+                {
+                    MessageBox.Show(item.GetPrintData("GR"));
+                }
+            }
 
-            //string a = "saba";
-            //string b = a;
-            //a = "tako";
-            //MessageBox.Show(b);
 
-            //if ( a == 5)
+            int k = 0;
+
+            #region MyRegion
+            ////int a = 5;
+
+
+            //// stack  -  value types      - Immutable way string
+            //// int , bool, double, float
+
+            ////heap  -  reference types    mutable way  - string
+            //// string, class , object
+
+
+            ////int b = 5;
+            ////int c = b;
+            ////b = 3;
+            ////int d = ++c;
+            ////c = 10;
+            ////MessageBox.Show("c = " + c.ToString());
+
+            ////Person p1 = new Person();
+            ////p1.FirstName = "giorgi";
+            ////p1.LastName = "gogokhia";
+            ////p1.BirthDate = new DateTime(1997, 7, 9);
+
+            ////Person p2 = new Person();
+            ////p2.FirstName = p1.FirstName;
+            ////MessageBox.Show("FirstName = " + p2.FirstName);
+
+
+            ////p1.LastName = "dea";
+
+            //bool k = true;
+            //bool t = false;
+            //if (t & t)
             //{
-            //    MessageBox.Show("a = 5");
-            //}
-            //else if ( a == 6 )
-            //{
-            //    MessageBox.Show("a = 6");
-            //}
-            //else if (a == 7)
-            //{
-            //    MessageBox.Show("a = 7");
+            //    MessageBox.Show("ok");
             //}
             //else
             //{
-            //    MessageBox.Show("a = 0");
+            //    MessageBox.Show("not ok");
             //}
 
-            //int x = a == 5 ? 1 : 0;
 
-            //int y = a++;
 
-            //a = a + 1;
-            //int y = a;
+            ////string a = "saba";
+            ////string b = a;
+            ////a = "tako";
+            ////MessageBox.Show(b);
 
-            //int y = a;
-            //a = a + 1;
+            ////if ( a == 5)
+            ////{
+            ////    MessageBox.Show("a = 5");
+            ////}
+            ////else if ( a == 6 )
+            ////{
+            ////    MessageBox.Show("a = 6");
+            ////}
+            ////else if (a == 7)
+            ////{
+            ////    MessageBox.Show("a = 7");
+            ////}
+            ////else
+            ////{
+            ////    MessageBox.Show("a = 0");
+            ////}
 
-            //a /= 5;
-            //double result = Convert.ToDouble(a) / 5;
-            //switch (a)
-            //{
-            //    case 5:
-            //        MessageBox.Show("a = 5");
-            //        break;
-            //        goto case 7;
-            //    case 6:
-            //    case 9:
-            //        MessageBox.Show("a = 6 or 9");
-            //        break;
-            //    case 7:
-            //        MessageBox.Show("a = 7");
-            //        break;
-            //    default:
-            //        MessageBox.Show("a = 0");
-            //        break;
+            ////int x = a == 5 ? 1 : 0;
 
-            //}
+            ////int y = a++;
+
+            ////a = a + 1;
+            ////int y = a;
+
+            ////int y = a;
+            ////a = a + 1;
+
+            ////a /= 5;
+            ////double result = Convert.ToDouble(a) / 5;
+            ////switch (a)
+            ////{
+            ////    case 5:
+            ////        MessageBox.Show("a = 5");
+            ////        break;
+            ////        goto case 7;
+            ////    case 6:
+            ////    case 9:
+            ////        MessageBox.Show("a = 6 or 9");
+            ////        break;
+            ////    case 7:
+            ////        MessageBox.Show("a = 7");
+            ////        break;
+            ////    default:
+            ////        MessageBox.Show("a = 0");
+            ////        break;
+
+            ////} 
+            #endregion
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            sum = Convert.ToInt32(txt_number1.Text) + Convert.ToInt32(txt_number2.Text);
-            lbl_sum.Text = sum.ToString();
+            //sum = Convert.ToInt32(txt_number1.Text) + Convert.ToInt32(txt_number2.Text);
+            //lbl_sum.Text = sum.ToString();
         }
     }
 }
--- HelloWorldApp/Person.cs
+++ HelloWorldApp/Person.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace HelloWorldApp
 {
@@ -11,6 +12,81 @@ namespace HelloWorldApp
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime BirthDate { get; set; }
+        public int Height { get; set; }
+        public Person() { }
+        public Person(string firstName, string lastName, int height, DateTime birthDate)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Height = height;
+            this.BirthDate = birthDate;
+        }
+        public void PrintInfo()
+        {
+            MessageBox.Show("Person is : " +LastName + " " + FirstName);
+        }
+
+        public void PrintInfo(string language)
+        {
+            if (language == "GE")
+            {
+                MessageBox.Show("ეს ადამიანი არის : " + LastName + " " + FirstName);
+            }
+            else
+            {
+                MessageBox.Show("Person is : " + LastName + " " + FirstName);
+            }
+        }
+
+        public string GetPrintData(string language)
+        {
+            if (language == "GE")
+            {
+                return "ეს ადამიანი არის : " + LastName + " " + FirstName;
+            }
+            else
+            {
+                return "Person is : " + LastName + " " + FirstName;
+            }
+        }
+        public int CalculatePersonAge()
+        {
+            int age = DateTime.Now.Year - this.BirthDate.Year;
+            return age;
+        }
+
+        public int CalculateCharsInPerson()
+        {
+            return (FirstName + LastName).Length;
+        }
+
+        public bool IsOddCharsInFirstName()
+        {
+            return FirstName.Length % 2 == 1;
+        }
+
+        public void PrintInfo(int countryZipCode)
+        {
+            if (countryZipCode == 45254)
+            {
+                MessageBox.Show("ეს ადამიანი არის : " + LastName + " " + FirstName);
+            }
+            else
+            {
+                MessageBox.Show("Person is : " + LastName + " " + FirstName);
+            }
+        }
+        public void PrintInfo(string language,int countryZipCode)
+        {
+            if (countryZipCode == 45254 && language == "GE")
+            {
+                MessageBox.Show("ეს ადამიანი არის : " + LastName + " " + FirstName);
+            }
+            else
+            {
+                MessageBox.Show("Person is : " + LastName + " " + FirstName);
+            }
+        }
 
     }
 }
